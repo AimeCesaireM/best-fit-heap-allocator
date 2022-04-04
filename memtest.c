3330602b5bbd2cbcbcc2453a739ca60dda4aa3e0
Cesaire <cmugishawayo25@amherst.edu>
2022-04-04 01:12:43 +0000

    Final Code

--- memtest.c
+++ memtest.c
@@ -3,17 +3,19 @@
 
 int main (int argc, char **argv){
 
-  char* x = malloc(24);
-  char* y = malloc(19);
-  char* z = malloc(32);
+  char* alloc1 = malloc(100);
+  char* alloc2 = malloc(200);
+  char* alloc3 = malloc(99);
   
-  printf("x = %p\n", x);
-  printf("y = %p\n", y);
-  printf("z = %p\n", z);
+  printf("alloc1 = %p\n", alloc1);
+  printf("alloc2 = %p\n", alloc2);
+  printf("alloc3 = %p\n", alloc3);
 
-  char* a = realloc(x, 20);
-  char* b = realloc(x, 30);
-  printf("a = %p\n", a);
-  printf("b = %p\n", b);
+  char* realloc1 = realloc(alloc1, 140); //should be different address
+  char* realloc2 = realloc(alloc2, 30);  //should be same address, different block size
+  char* realloc3 = realloc(alloc3, 99);  //should be same address, same block size
+  printf("realloc1 = %p\n", realloc1);
+  printf("realloc2 = %p\n", realloc2);
+  printf("realloc3 = %p\n", realloc3);
   
 }
